5ba78665e101decdb3b76e23d39943dda8990226
MaxEdgington <edgington.m.w@gmail.com>
2023-08-24 18:48:11 -0400

    paramaterize students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,11 +13,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.cohort} :${user.teacher}`);
-    });
-  });
+`);
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values);
+
